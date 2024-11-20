1e8c8a3a226dbbeff603cc3d303ec5882937dfdc
erisbarrancop <ibarpri1712@iesfuengirola1.es>
2024-11-20 09:56:52 +0100

    Made the banner block be editable through the admin editor, and only allowing a few blocks to be used in it, which are the list, and the heading (and paragraph but that's by default)

--- build/banner.asset.php
+++ build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => '7af2a8252d34487994ba');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor'), 'version' => 'df2a4f5278f72866de1d');
--- our-blocks/banner.js
+++ our-blocks/banner.js
@@ -1,3 +1,5 @@
+import { InnerBlocks } from "@wordpress/block-editor"
+
 wp.blocks.registerBlockType("ourblocktheme/banner", {
     title: "Banner",
     edit: EditComponent,
@@ -5,15 +7,22 @@ wp.blocks.registerBlockType("ourblocktheme/banner", {
 })
 
 function EditComponent(){
-    return (
-        <div className="page-banner">
-            <div className="page-banner__bg-image" style={{backgroundImage: "url('/wordpress/wp-content/themes/fictional-block-theme/images/library-hero.jpg')"}}></div>
-            <div className="page-banner__content container t-center c-white">
-                <h1 className="headline headline--large">Welcome!</h1>
+    const useLater = (
+        <>
+        <h1 className="headline headline--large">Welcome!</h1>
                 <h2 className="headline headline--medium">We think you&rsquo;ll like it here.</h2>
                 <h3 className="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re
                     interested in?</h3>
                 <a href="#" className="btn btn--large btn--blue">Find Your Major</a>
+        </>
+    )
+    
+    return (
+        <div className="page-banner">
+            <div className="page-banner__bg-image" style={{backgroundImage: "url('/wordpress/wp-content/themes/fictional-block-theme/images/library-hero.jpg')"}}></div>
+            <div className="page-banner__content container t-center c-white">
+                <InnerBlocks allowedBlocks={["core/paragraph", "core/heading", "core/list"]}/>
+                
             </div>
         </div>
     )
@@ -21,5 +30,11 @@ function EditComponent(){
 
 
 function SaveComponent(){
-    return <p>This comes from our block</p>
+    return (<div className="page-banner">
+        <div className="page-banner__bg-image" style={{backgroundImage: "url('/wordpress/wp-content/themes/fictional-block-theme/images/library-hero.jpg')"}}></div>
+        <div className="page-banner__content container t-center c-white">
+            <InnerBlocks.Content />
+            
+        </div>
+    </div>)
 }
