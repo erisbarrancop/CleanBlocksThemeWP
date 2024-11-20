ff6316860253a825a599db8e08cc31dda81c9047
erisbarrancop <ibarpri1712@iesfuengirola1.es>
2024-11-20 13:20:23 +0100

    Started working on the themes.json file, that will allow us to customize the native look of WP's block theme, in this case i added some theme colors and made the button use on of those by default

--- build/banner.asset.php
+++ build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '38325076ddabf6dcb397');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '683eec03be9e02682cc1');
