677111db730c02aeb5044ef061b3a5141b54689e
erisbarrancop <ibarpri1712@iesfuengirola1.es>
2024-11-20 15:49:00 +0100

    Added final touches to the button, now it will also display a link icon to set the href to whatever page on our site we want

--- build/genericbutton.asset.php
+++ build/genericbutton.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '80faff27b769fb49a705');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'b8bc230d32e8fea3f0d5');
--- our-blocks/genericbutton.js
+++ our-blocks/genericbutton.js
@@ -1,25 +1,41 @@
-import { ToolbarGroup, ToolbarButton } from "@wordpress/components"
-import { RichText, BlockControls } from "@wordpress/block-editor"
+import { link } from "@wordpress/icons"
+import { ToolbarGroup, ToolbarButton, Popover, Button } from "@wordpress/components"
+import { RichText, BlockControls, __experimentalLinkControl as LinkControl} from "@wordpress/block-editor"
 import { registerBlockType } from "@wordpress/blocks"
+import { useState } from "@wordpress/element"
 
 registerBlockType("ourblocktheme/genericbutton", {
     title: "Generic Button",
     attributes: {
         text: {type:"string"},
-        size: {type:"string", default:"large"}
+        size: {type:"string", default:"large"},
+        linkObject: { type: "object"}
     },
     edit: EditComponent,
     save: SaveComponent
 })
 
 function EditComponent(props){
+    const [isLinkPickerVisible, setIsLinkPickerVisible] = useState(false)
+
     function handleTextChange(x){
         props.setAttributes({text: x})
+    }
+
+    function buttonHandler(){
+        setIsLinkPickerVisible(prev => !prev)
+    }
+
+    function handleLinkChange(newLink){
+        props.setAttributes({linkObject: newLink})
     }
 
     return (
         <>
             <BlockControls>
+                <ToolbarGroup>
+                    <ToolbarButton onClick={buttonHandler} icon={link}/>
+                </ToolbarGroup>
                 <ToolbarGroup>
                     <ToolbarButton isPressed={props.attributes.size === "large"} onClick={() => props.setAttributes({size: "large"})}>
                         Large
@@ -32,7 +48,13 @@ function EditComponent(props){
                     </ToolbarButton>
                 </ToolbarGroup>
             </BlockControls>
-            <RichText allowedFormats={[""]} tagName="a" className={`btn btn--${props.attributes.size}`} value={props.attributes.text} onChange={handleTextChange}/>
+            <RichText allowedFormats={[""]} tagName="a" className={`btn btn--${props.attributes.size} btn--blue`} value={props.attributes.text} onChange={handleTextChange}/>
+            {isLinkPickerVisible && (
+                <Popover position="middle center">
+                    <LinkControl settings={[]} value={props.attributes.linkObject} onChange={handleLinkChange}/>
+                    <Button variant="primary" onClick={() => setIsLinkPickerVisible(false)} style={{display:"block", width:"100%"}} >Confirm Link</Button>
+                </Popover>
+            )}
         </>
     )
 }
@@ -40,7 +62,7 @@ function EditComponent(props){
 
 function SaveComponent(props){
     return ( 
-    <a href="#" className={`btn btn--${props.attributes.size}`}>
+    <a href={props.attributes.linkObject.url} className={`btn btn--${props.attributes.size}`}>
         {props.attributes.text}
     </a>
     )
